701cdf1f2ba92be07df8c78ac4746a7459f6af46
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-04-16 11:50:33 +0300

    Add LinkStorage installer.

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -73,6 +73,7 @@ class NewCommand extends SymfonyCommand
             Installation\AddAppProviderToConfiguration::class,
             Installation\RunNpmInstall::class,
             Installation\RunGulp::class,
+            Installation\LinkStorage::class,
         ];
 
         foreach ($installers as $installer) {
